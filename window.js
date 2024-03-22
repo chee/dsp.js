1e80182b12edd5f25ca63c530f74832b371f8401
chee <yay@chee.party>
2024-03-22 18:26:30 +0000

    allow windows to be applied by map

--- window.js
+++ window.js
@@ -1,76 +1,71 @@
-/**
- * @param {number} index
- * @param {number} length
- */
-export let bartlett = (index, length) =>
-	(2 / (length - 1)) * ((length - 1) / 2 - Math.abs(index - (length - 1) / 2))
+import {TWO_PI} from "./constants.js"
 
 /**
+ * @param {number} value
  * @param {number} index
- * @param {number} length
+ * @param {Float32Array} array
  */
-export let bartlettHann = (index, length) =>
-	0.62 -
-	0.48 * Math.abs(index / (length - 1) - 0.5) -
-	0.38 * Math.cos((DSP.TWO_PI * index) / (length - 1))
+export let bartlett = (value, index, array) =>
+	value *
+	(2 / (array.length - 1)) *
+	((array.length - 1) / 2 - Math.abs(index - (array.length - 1) / 2))
 
 /**
+ * @param {number} value
  * @param {number} index
- * @param {number} length
+ * @param {Float32Array} array
  */
-export let blackman = (index, length, alpha = 0.16) => {
-	let a0 = (1 - alpha) / 2
-	let a1 = 0.5
-	let a2 = alpha / 2
+export let bartlettHann = (value, index, array) =>
+	value * 0.62 -
+	0.48 * Math.abs(index / (array.length - 1) - 0.5) -
+	0.38 * Math.cos((TWO_PI * index) / (array.length - 1))
 
-	return (
-		a0 -
-		a1 * Math.cos((DSP.TWO_PI * index) / (length - 1)) +
-		a2 * Math.cos((4 * Math.PI * index) / (length - 1))
-	)
-}
 /**
+ * @param {number} value
  * @param {number} index
- * @param {number} length
+ * @param {Float32Array} array
  */
-export let cosine = (index, length) =>
-	Math.cos((Math.PI * index) / (length - 1) - Math.PI / 2)
+export let cosine = (value, index, array) =>
+	value * Math.cos((Math.PI * index) / (array.length - 1) - Math.PI / 2)
+
 /**
+ * @param {number} value
  * @param {number} index
- * @param {number} length
+ * @param {Float32Array} array
  */
-export let gauss = (index, length, alpha = 0.25) =>
-	Math.pow(
-		Math.E,
-		-0.5 *
-			Math.pow((index - (length - 1) / 2) / ((alpha * (length - 1)) / 2), 2)
-	)
+export let hamming = (value, index, array) =>
+	value * 0.54 - 0.46 * Math.cos((TWO_PI * index) / (array.length - 1))
 /**
+ * @param {number} value
  * @param {number} index
- * @param {number} length
+ * @param {Float32Array} array
  */
-export let hamming = (index, length) =>
-	0.54 - 0.46 * Math.cos((DSP.TWO_PI * index) / (length - 1))
+export let hann = (value, index, array) =>
+	value * 0.5 * (1 - Math.cos((TWO_PI * index) / (array.length - 1)))
+
 /**
+ * @param {number} value
  * @param {number} index
- * @param {number} length
+ * @param {Float32Array} array
  */
-export let hann = (index, length) =>
-	0.5 * (1 - Math.cos((DSP.TWO_PI * index) / (length - 1)))
-/**
- * @param {number} index
- * @param {number} length
- */
-export let lanczos = (index, length) => {
-	let x = (2 * index) / (length - 1) - 1
-	return Math.sin(Math.PI * x) / (Math.PI * x)
+export let lanczos = (value, index, array) => {
+	let x = (2 * index) / (array.length - 1) - 1
+	return (value * Math.sin(Math.PI * x)) / (Math.PI * x)
 }
 
-export let rectangular = () => 1
+/**
+ * @param {number} value
+ * @param {number} _index
+ * @param {Float32Array} _array
+ */
+export let rectangular = (value, _index, _array) => value
 
 /**
+ * @param {number} value
  * @param {number} index
- * @param {number} length
+ * @param {Float32Array} array
  */
-export let triangular = (index, length) =>
-	(2 / length) * (length / 2 - Math.abs(index - (length - 1) / 2))
+export let triangular = (value, index, array) =>
+	value *
+	(2 / array.length) *
+	(array.length / 2 - Math.abs(index - (array.length - 1) / 2))
